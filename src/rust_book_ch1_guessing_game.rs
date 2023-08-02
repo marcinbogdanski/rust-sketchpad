90b01cdac1736dc90d8af5a48355cb267ffa8b93
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2023-08-02 15:47:10 +0200

    Add random number generator

--- src/rust_book_ch1_guessing_game.rs
+++ src/rust_book_ch1_guessing_game.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 pub fn main_guessing_game() {
     println!("Guessing Game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=101);    // range
+
+    println!("The secret number is: {secret_number}");
 
     let mut guess = String::new();
 
@@ -9,5 +15,16 @@ pub fn main_guessing_game() {
         .read_line(&mut guess)           // pass mutable reference
         .expect("Failed to read line");  // crash or return number of bytes read
 
+    let guess: u32 = guess                    // mark as u32
+        .trim()                               // remove newline
+        .parse()                              // parse to u32
+        .expect("Please type a number!");     // crash or return number
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
